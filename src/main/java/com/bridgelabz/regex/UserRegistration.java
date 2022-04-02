fbace8ad3a6d8d8ebccdd11122b2d99dc705597c
Aniket1675 <Aniket@05>
2022-04-02 18:04:25 +0530

    Added test case for to check first name is correct or wrong

--- src/main/java/com/bridgelabz/regex/UserRegistration.java
+++ src/main/java/com/bridgelabz/regex/UserRegistration.java
@@ -10,5 +10,26 @@ import java.util.regex.Pattern;
  * */
 
 public class UserRegistration {
+    // (?=.*[a-z]) # a lower case letter must occur at least once
+    // (?=.*[A-Z]) # an upper case letter must occur at least once
+    // (?=.*[0-9]) # a digit must occur at least once
+    // (?=.*[@#$%^&+=]) # a special character must occur at least once
+    // (?=\S+$) # no whitespace allowed in the entire string
+    // .{8,} # anything, at least eight places though
+
+
+    /*
+     Added  first name method to check first name
+    */
+    public boolean firstName(String firstName) {
+        boolean result  = Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName) ;
+
+        if (result) {
+            System.out.println("The first name " + firstName + " is valid.");
+        } else {
+            System.out.println("The first name " + firstName + " is invalid.");
+        }
+        return result;
+    }
 
 }
--- src/test/java/com/bridgelabz/regex/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/regex/UserRegistrationTest.java
@@ -1,6 +1,27 @@
 package com.bridgelabz.regex;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
-public class UserRegistrationTest {
+/**
+ * Program on User Registration Using Regex
+ * purpose : performing the test cases of all method
+ * @author : Aniket
+ * @since : 01/04/2022
+ * */
+    public class UserRegistrationTest {
+    UserRegistration userRegistration = new UserRegistration();
+
+    @Test
+    public void givenfirstName_WhenProper_ShouldReturnTrue(){
+        boolean result = userRegistration.firstName("Aniket");
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void givenfirstName_WhenNotProper_ShouldReturnFalse(){
+        boolean result = userRegistration.firstName("aniket");
+        Assert.assertFalse(result);
+    }
+
 
 }
